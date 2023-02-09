1544ebf7973153160faf7a27a219fc8e5d5e04b0
Josue Jeronimo <josuejero@hotmail.com>
2023-02-09 18:14:08 -0500

    final update

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 
 const root = createRoot(document.getElementById('root'))
