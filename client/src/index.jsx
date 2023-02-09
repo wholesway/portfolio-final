f7379ece8b51c4adbcc40ef40726ea31ec1ffc11
Josue Jeronimo <josuejero@hotmail.com>
2023-02-09 18:03:47 -0500

    Initial commit

---
+++ client/src/index.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom'
+import App from './App'
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
+
